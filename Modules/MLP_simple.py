6c8b55d78ee9b255b38b257c21446f17af56411b
coldplaying42 <mzyone@gmail.com>
2022-06-02 13:42:04 -0400

    revise simple MLP

--- Modules/MLP_simple.py
+++ Modules/MLP_simple.py
@@ -15,25 +15,19 @@ import logging
 class simpleBlock(nn.Module):
     def __init__(self):
         super(simpleBlock, self).__init__()
-        self.affine_out = nn.Linear(in_features=256, out_features=1)
+        self.affine_out = nn.Linear(in_features=1536, out_features=1)
         self.sigmoid = nn.Sigmoid()
         self.init_weights()
 
     def init_weights(self):
         init.xavier_normal_(self.affine_out.weight)
 
-    def forward(self, query_emb, profile_emb):
-        query_V = torch.mean(query_emb, dim=2)
-        #print("query_V: ", query_V.shape)
+    def forward(self, query_emb, profile_emb, query_mask, profile_mask):
 
-        profile_V = torch.mean(profile_emb, dim=2)
-        #print("profile_V: ", profile_V.shape)
-
+        query_V = torch.mean(query_emb, dim=1)
+        profile_V = torch.mean(profile_emb, dim=1)
         all_concat = torch.cat([query_V, profile_V], dim=-1)
-        #print("all_concat: ", all_concat.shape)
         matching_output = self.affine_out(all_concat)
-
-        #print("before sigmoid: ", matching_output)
         output = self.sigmoid(matching_output)
         return output.squeeze()
 
@@ -51,13 +45,10 @@ class ourModel (nn.Module):
     
     def forward(self, batch_query_emb, batch_profile_emb, batch_dialogs_emb, \
          batch_query_mask, batch_profile_mask, batch_dialogs_mask):
-        
-        output = self.simpleBlock(batch_query_emb, batch_profile_emb)
-
+        output = self.simpleBlock(batch_query_emb, batch_profile_emb, batch_query_mask, batch_profile_mask)
         return output.squeeze()
 
 def train_epoch(train_dataloader, optimizer, model, tag):
-
     epoch_loss = []
     loss_fun = nn.BCELoss()
     for batch, labels in tqdm(train_dataloader):
@@ -67,7 +58,6 @@ def train_epoch(train_dataloader, optimizer, model, tag):
         batch_profile_embed, batch_profile_attention_mask = batch[0], batch[1].float()
         batch_query_embed, batch_query_attention_mask = batch[2], batch[3].float()
         batch_dialogs_embed, batch_dialogs_attention_mask = batch[4], batch[5].float()
-        # print("batch_dialogs_attnetion_mask", batch_dialogs_attention_mask.shape)
         batch_size, dr_dialog_num, max_len = batch_dialogs_embed.shape
 
         batch_dialogs_mask = model.get_dialog_sent_masks(batch_dialogs_attention_mask).float()
@@ -75,10 +65,10 @@ def train_epoch(train_dataloader, optimizer, model, tag):
         if tag == "train":
             optimizer.zero_grad()
         logits = model(batch_query_embed, batch_profile_embed, batch_dialogs_embed, batch_query_attention_mask, batch_profile_attention_mask, batch_dialogs_mask)
-        print("labels", labels)
-        print("logits", logits)
+        #print("labels", labels)
+        #print("logits", logits)
         loss = loss_fun(logits, labels)
-        print("loss", loss)
+        #print("loss", loss)
 
         if tag == "train":
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=20.0, norm_type=2)
@@ -88,7 +78,6 @@ def train_epoch(train_dataloader, optimizer, model, tag):
     return epoch_loss
 
 def test_process(test_dataloader, model):
-
     logits_list = []
     num_ok = 0
     total_num = 0
@@ -99,36 +88,12 @@ def test_process(test_dataloader, model):
         batch_query_emb, batch_query_attention_mask = batch[2], batch[3].float()
         batch_dialogs_emb, batch_dialogs_attention_mask = batch[4], batch[5].float()
         batch_size, dr_dialog_num, max_len = batch_dialogs_emb.shape
-
         batch_dialogs_mask = model.get_dialog_sent_masks(batch_dialogs_attention_mask).float()
 
-        print("batch_profile_emb: ", batch_profile_emb.shape)
-        print("batch_query_emb: ", batch_query_emb.shape)
-        print("batch_dialogs_emb: ", batch_dialogs_emb.shape)
-
-        print("batch_profile_attention_mask: ", batch_profile_attention_mask.shape)
-        print("batch_query_attention_mask: ", batch_query_attention_mask.shape)
-        print("batch_dialogs_attention_mask: ", batch_dialogs_attention_mask.shape)
-
-        print("batch_dialogs_mask: ", batch_dialogs_mask.shape)
-
-        print("batch_profile_emb: ", batch_profile_emb)
-        print("batch_query_emb: ", batch_query_emb)
-        print("batch_dialogs_emb: ", batch_dialogs_emb)
-
-        print("batch_profile_attention_mask: ", batch_profile_attention_mask)
-        print("batch_query_attention_mask: ", batch_query_attention_mask)
-        print("batch_dialogs_attention_mask: ", batch_dialogs_attention_mask)
-
-        print("batch_dialogs_mask: ", batch_dialogs_mask)
-
-
-        # print("batch_dialog_emb: ", batch_dialogs_emb.shape)
         logits = model(batch_query_emb, batch_profile_emb, batch_dialogs_emb, batch_query_attention_mask, batch_profile_attention_mask, batch_dialogs_mask)
         print("logits: ", logits)
         total_num += 1
-        # print("logits", logits.shape)
-        # print("logits", logits)
+
         logits_list.append(logits)
         if logits.float().argmax(dim=0) == 0:
             num_ok += 1
