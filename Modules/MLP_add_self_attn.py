592275d52c98288191edc4f91e056a6a7bcb6a6a
Wei Yuan <eway.weiyuan@hotmail.com>
2022-06-10 21:05:36 -0400

    add version only rely on self-attn

---
+++ Modules/MLP_add_self_attn.py
@@ -0,0 +1,304 @@
+import os
+
+import torch
+from torch.utils.data import DataLoader, RandomSampler, TensorDataset
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+from tqdm import tqdm
+
+from transformers import AdamW, BertModel, BertTokenizer, get_linear_schedule_with_warmup
+
+
+
+class TransformerBlock(nn.Module):
+
+    def __init__(self, input_size, is_layer_norm=False):
+        super(TransformerBlock, self).__init__()
+        self.is_layer_norm = is_layer_norm
+        if is_layer_norm:
+            self.layer_morm = nn.LayerNorm(normalized_shape=input_size)
+
+        self.relu = nn.ReLU()
+        self.linear1 = nn.Linear(input_size, input_size)
+        self.linear2 = nn.Linear(input_size, input_size)
+        self.init_weights()
+
+    def init_weights(self): 
+        init.xavier_normal_(self.linear1.weight)
+        init.xavier_normal_(self.linear2.weight)
+
+    def forward(self, Q, K, V, mask=None, dropout=None, episilon=1e-8):
+        '''
+        :param Q: (batch_size, max_r_words, embedding_dim)
+        :param K: (batch_size, max_u_words, embedding_dim)
+        :param V: (batch_size, max_u_words, embedding_dim)
+        :return: output: (batch_size, max_r_words, embedding_dim)  same size as Q
+        '''
+        dk = torch.Tensor([max(1.0, Q.size(-1))]).cuda()
+
+        Q_K = Q.bmm(K.permute(0, 2, 1)) / (torch.sqrt(dk) + episilon)
+        if mask is not None:
+            Q_K = Q_K.masked_fill_(mask, -1e9)
+        Q_K_score = F.softmax(Q_K, dim=-1)  # (batch_size, max_r_words, max_u_words)
+        if dropout is not None:
+            Q_K_score = dropout(Q_K_score)
+
+        V_att = Q_K_score.bmm(V)
+
+        if self.is_layer_norm:
+            X = self.layer_morm(Q + V_att)  # (batch_size, max_r_words, embedding_dim)
+            output = self.layer_morm(self.FFN(X) + X)
+        else:
+            X = Q + V_att
+            X = self.linear2(self.relu(self.linear1(X))) + X
+
+        return X
+
+class cnnBlock(nn.Module):
+    def __init__(self):
+        super(cnnBlock, self).__init__()
+        self.cnn_2d_query_profile_1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3,3))
+        self.cnn_2d_query_profile_2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3,3))
+        self.maxpooling_query_profile_1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(1, 1))
+        self.maxpooling_query_profile_2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(1, 1))
+        self.affine_query_profile = nn.Linear(in_features=15625, out_features=2000)
+        self.relu = nn.ReLU()
+
+        self.cnn_2d_query_dialogs_1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3,3))
+        self.cnn_2d_query_dialogs_2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3,3))
+        self.maxpooling_query_dialogs_1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(1, 1))
+        self.maxpooling_query_dialogs_2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(1, 1))
+        self.affine_query_dialogs = nn.Linear(in_features=12125, out_features=2000)
+        
+        self.bn1 = nn.BatchNorm1d(num_features=768)
+        self.fc1 = nn.Linear(in_features =2000 * 4, out_features = 768)
+        self.fc2 = nn.Linear(768, 1)
+
+        self.sigmoid = nn.Sigmoid()
+
+        self.init_weights()
+
+    def init_weights(self):
+        init.xavier_normal_(self.cnn_2d_query_profile_1.weight)
+        # init.xavier_normal_(self.cnn_2d_query_profile_2.weight)
+        init.xavier_normal_(self.affine_query_profile.weight)
+
+        init.xavier_normal_(self.cnn_2d_query_dialogs_1.weight)
+        # init.xavier_normal_(self.cnn_2d_query_dialogs_2.weight)
+        init.xavier_normal_(self.affine_query_dialogs.weight)
+        
+        init.xavier_normal_(self.fc1.weight)
+        init.xavier_normal_(self.fc2.weight)
+
+
+
+    def cnn_query_profile(self, matrix):
+
+        matrix = matrix.unsqueeze(1)
+        # print("cnn_query_profile matrix: ", matrix)
+        # print("self.relu(self.cnn_2d_query_profile_1(matrix)): ", self.relu(self.cnn_2d_query_profile_1(matrix)).shape)
+        Z = self.relu(self.cnn_2d_query_profile_1(matrix))
+        # print("cnn_query_profile Z1: ", Z)
+        Z = self.maxpooling_query_profile_1(Z)
+        # print("cnn_query_profile Z2: ", Z)
+        Z = Z.view(Z.size(0), -1)
+
+        output_V = self.affine_query_profile(Z)
+        return output_V
+
+    def cnn_query_dialogs(self, matrix):
+        matrix = matrix.unsqueeze(1)
+        Z = self.relu(self.cnn_2d_query_dialogs_1(matrix))
+        # print("cnn_query_dialogs Z1: ", Z)
+        Z = self.maxpooling_query_dialogs_1(Z)
+        # print("cnn_query_dialogs Z2: ", Z)
+        Z = Z.view(Z.size(0), -1)
+        output_V = self.affine_query_dialogs(Z)
+        return output_V
+
+    def forward(self, query_profile_similarity_matrix, query_dialogs_similarity_matrix,\
+                query_profile_interaction_simialrity_matrix, query_dialogs_interaction_simialrity_matrix):
+        
+        query_profile_V = self.cnn_query_profile(query_profile_similarity_matrix)
+        query_profile_interaction_V = self.cnn_query_profile(query_profile_interaction_simialrity_matrix)
+
+        query_dialogs_V = self.cnn_query_dialogs(query_dialogs_similarity_matrix)
+        query_dialogs_interaction_V = self.cnn_query_dialogs(query_dialogs_interaction_simialrity_matrix)
+
+        # print("query_profile_V: ", query_profile_V)
+        # print("query_profile_interaction_V: ", query_profile_interaction_V)
+        # print("query_dialogs_V: ", query_dialogs_V)
+        # print("query_dialogs_interaction_V: ", query_dialogs_interaction_V)
+        features = torch.cat([query_profile_V, query_dialogs_V, query_profile_interaction_V, query_dialogs_interaction_V], dim=-1)
+        features = self.relu(self.bn1(self.fc1(features)))
+        output = self.fc2(features)
+        output = self.sigmoid(output)
+        # print("output: ", output.squeeze())
+        return output.squeeze()
+
+class ourModel (nn.Module):
+    def __init__(self):
+        super(ourModel, self).__init__()
+        self.cnn_block = cnnBlock()
+        self.embed_dim = 768
+        self.query_self_transformer  = TransformerBlock(input_size=self.embed_dim)
+        self.profile_self_transformer = TransformerBlock(input_size=self.embed_dim)
+        self.dialogs_self_transformer = TransformerBlock(input_size=self.embed_dim)
+
+        self.query_profile_transformer  = TransformerBlock(input_size=self.embed_dim)
+        self.query_dialogs_transformer  = TransformerBlock(input_size=self.embed_dim)
+        self.profile_transformer = TransformerBlock(input_size=self.embed_dim)
+        self.dialogs_transformer = TransformerBlock(input_size=self.embed_dim)
+
+
+
+        
+    def get_dialog_sent_masks(self, batch_dialogs_attention_mask):
+        batch_dialogs_mask = []
+        for i in range(batch_dialogs_attention_mask.shape[0]):
+            batch_dialogs_mask.append(torch.where(torch.sum(batch_dialogs_attention_mask[i], dim=1) != 0, torch.tensor(1).cuda(), torch.tensor(0).cuda()))
+        batch_dialogs_mask = torch.stack(batch_dialogs_mask)
+        return batch_dialogs_mask
+    
+    def forward(self, batch_query_emb, batch_profile_emb, batch_dialogs_emb, \
+         batch_query_mask, batch_profile_mask, batch_dialogs_mask):
+
+        batch_query_emb = batch_query_emb.masked_fill(batch_query_mask.unsqueeze(-1)==0, 5e-5)
+        batch_profile_emb = batch_profile_emb.masked_fill(batch_profile_mask.unsqueeze(-1)==0, 5e-5)
+        batch_dialogs_emb = batch_dialogs_emb.masked_fill(batch_dialogs_mask.unsqueeze(-1)==0, 5e-5)
+
+
+        batch_query_emb_norm = torch.nn.functional.normalize(batch_query_emb, dim=-1) 
+        batch_profile_emb_norm = torch.nn.functional.normalize(batch_profile_emb, dim=-1) 
+        batch_dialogs_emb_norm = torch.nn.functional.normalize(batch_dialogs_emb, dim=-1) 
+
+        query_self_attn_mask_ = ~torch.bmm(batch_query_mask.unsqueeze(-1), batch_query_mask.unsqueeze(1)).bool()
+        profile_self_attn_mask_ = ~torch.bmm(batch_profile_mask.unsqueeze(-1), batch_profile_mask.unsqueeze(1)).bool()
+        dialogs_self_attn_mask_ = ~torch.bmm(batch_dialogs_mask.unsqueeze(-1), batch_dialogs_mask.unsqueeze(1)).bool()
+
+        batch_query_self_attn = self.query_self_transformer(batch_query_emb_norm, batch_query_emb_norm, batch_query_emb_norm, query_self_attn_mask_)
+        batch_profile_self_attn = self.profile_self_transformer(batch_profile_emb_norm, batch_profile_emb_norm, batch_profile_emb_norm, profile_self_attn_mask_)
+        batch_dialogs_self_attn = self.dialogs_self_transformer(batch_dialogs_emb_norm, batch_dialogs_emb_norm, batch_dialogs_emb_norm, dialogs_self_attn_mask_)
+
+        query_profile_similarity_matrix = torch.bmm(batch_query_self_attn, batch_profile_self_attn.transpose(1,2))
+
+        query_dialogs_similarity_matrix = torch.bmm(batch_query_self_attn, batch_dialogs_self_attn.transpose(1,2))
+
+        # 2.Interaction
+        query_profile_attn_mask_ = ~torch.bmm(batch_query_mask.unsqueeze(-1), batch_profile_mask.unsqueeze(1)).bool()
+        query_dialogs_attn_mask_ = ~torch.bmm(batch_query_mask.unsqueeze(-1), batch_dialogs_mask.unsqueeze(1)).bool()
+        
+        # 2.1 Calcuate attn
+        query_add_profile_attn = self.query_profile_transformer(batch_query_self_attn, batch_profile_self_attn, batch_profile_self_attn, mask=~query_profile_attn_mask_)
+        profile_add_query_attn = self.profile_transformer(batch_profile_self_attn, batch_query_self_attn, batch_query_self_attn, mask=~query_profile_attn_mask_.transpose(1,2))
+
+        query_add_dialogs_attn = self.query_dialogs_transformer(batch_query_self_attn, batch_dialogs_self_attn, batch_dialogs_self_attn, mask=~query_dialogs_attn_mask_)
+        dialogs_add_query_attn = self.dialogs_transformer(batch_dialogs_self_attn, batch_query_self_attn, batch_query_self_attn, mask=~query_dialogs_attn_mask_.transpose(1,2))
+
+        # 2.2 Calcuate query-profile attn interaction
+        query_profile_interaction_simialrity_matrix = torch.bmm(query_add_profile_attn, profile_add_query_attn.transpose(1,2))
+
+        # 2.3 Calcuate query-dialogs attn interaction
+        query_dialogs_interaction_simialrity_matrix = torch.bmm(query_add_dialogs_attn, dialogs_add_query_attn.transpose(1,2))
+
+        # print("query_profile_similarity_matrix: ", query_profile_similarity_matrix)
+        # print("query_dialogs_similarity_matrix: ", query_dialogs_similarity_matrix)
+        # print("query_profile_interaction_simialrity_matrix: ", query_profile_interaction_simialrity_matrix)
+        # print("query_dialogs_interaction_simialrity_matrix: ", query_dialogs_interaction_simialrity_matrix)
+
+        output = self.cnn_block(query_profile_similarity_matrix, query_dialogs_similarity_matrix,\
+                                query_profile_interaction_simialrity_matrix, query_dialogs_interaction_simialrity_matrix)
+
+        return output.squeeze()
+
+def train_epoch(train_dataloader, optimizer, model, tag):
+
+    epoch_loss = []
+    loss_fun = nn.BCELoss()
+    for batch, labels in tqdm(train_dataloader):
+        labels = labels.cuda()
+        # print("labels", labels)
+        batch = tuple(t.cuda() for t in batch)
+
+        batch_profile_embed, batch_profile_attention_mask = batch[0], batch[1].float()
+        batch_query_embed, batch_query_attention_mask = batch[2], batch[3].float()
+        batch_dialogs_embed, batch_dialogs_attention_mask = batch[4], batch[5].float()
+        # print("batch_dialogs_attnetion_mask", batch_dialogs_attention_mask.shape)
+        batch_size, dr_dialog_num, max_len = batch_dialogs_embed.shape
+
+        batch_dialogs_mask = model.get_dialog_sent_masks(batch_dialogs_attention_mask).float()
+ 
+        if tag == "train":
+            optimizer.zero_grad()
+        logits = model(batch_query_embed, batch_profile_embed, batch_dialogs_embed, batch_query_attention_mask, batch_profile_attention_mask, batch_dialogs_mask)
+        
+        loss = loss_fun(logits, labels)
+        # print("logits", logits)
+        # print("loss", loss)
+        # print("loss", loss)
+        # print("logits", logits)
+        # print("labels", labels)
+        if tag == "train":
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=20.0, norm_type=2)
+            loss.backward()
+            optimizer.step()
+        epoch_loss.append(loss.item())
+    return epoch_loss
+
+def valid_epoch(valid_dataloader, model, tag):
+
+    epoch_loss = []
+    loss_fun = nn.BCELoss()
+    total_num = 0
+    num_ok = 0
+    for batch, labels in tqdm(valid_dataloader):
+        labels = labels.cuda()
+        # print("labels", labels)
+        batch = tuple(t.cuda() for t in batch)
+
+        batch_profile_embed, batch_profile_attention_mask = batch[0], batch[1].float()
+        batch_query_embed, batch_query_attention_mask = batch[2], batch[3].float()
+        batch_dialogs_embed, batch_dialogs_attention_mask = batch[4], batch[5].float()
+        # print("batch_dialogs_attnetion_mask", batch_dialogs_attention_mask.shape)
+        batch_size, dr_dialog_num, max_len = batch_dialogs_embed.shape
+
+        batch_dialogs_mask = model.get_dialog_sent_masks(batch_dialogs_attention_mask).float()
+ 
+        logits = model(batch_query_embed, batch_profile_embed, batch_dialogs_embed, batch_query_attention_mask, batch_profile_attention_mask, batch_dialogs_mask)
+        if logits.float().argmax(dim=0) == 0:
+            num_ok += 1
+        total_num += 1
+        loss = loss_fun(logits, labels)
+        epoch_loss.append(loss.item())
+    print("Validation Accuracy: ", num_ok/total_num)
+    return epoch_loss
+    
+def test_process(test_dataloader, model):
+
+    logits_list = []
+    num_ok = 0
+    total_num = 0
+    for batch, labels in tqdm(test_dataloader):
+        batch = tuple(t.cuda() for t in batch)
+        labels = labels.cuda()
+        batch_profile_emb, batch_profile_attention_mask = batch[0], batch[1].float()
+        batch_query_emb, batch_query_attention_mask = batch[2], batch[3].float()
+        batch_dialogs_emb, batch_dialogs_attention_mask = batch[4], batch[5].float()
+        batch_size, dr_dialog_num, max_len = batch_dialogs_emb.shape
+
+        batch_dialogs_mask = model.get_dialog_sent_masks(batch_dialogs_attention_mask).float()
+
+
+        # print("batch_dialog_emb: ", batch_dialogs_emb.shape)
+        logits = model(batch_query_emb, batch_profile_emb, batch_dialogs_emb, batch_query_attention_mask, batch_profile_attention_mask, batch_dialogs_mask)
+        # print("logits: ", logits)
+        total_num += 1
+        # print("logits", logits.shape)
+        # print("labels", labels)
+        logits_list.append(logits)
+        if logits.float().argmax(dim=0) == 0:
+            num_ok += 1
+    print("Testing Accuracy: ", num_ok/total_num)
+    return logits_list
---
+++ train_add_self_attn.py
@@ -0,0 +1,131 @@
+import argparse
+import json
+from collections import Counter
+import random
+import os 
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from tqdm import tqdm, trange
+import time
+from Modules.MLP_add_self_attn import ourModel, train_epoch, valid_epoch, test_process
+from utils.dataset import DoctorRecDataset
+from utils.EarlyStopping import EarlyStopping
+from utils.loss import weighted_class_bceloss
+from utils.config import init_opts, train_opts, multihead_att_opts
+
+from utils.util import save_pickle, load_pickle
+from transformers import AdamW, BertModel, BertTokenizer, get_linear_schedule_with_warmup
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', default=2021, type=int)
+parser.add_argument('--gpu', default=0, type=int)
+parser.add_argument('--n_gpu', default=1, type=int)
+
+parser.add_argument('--name', default="med-bert", type=str)
+parser.add_argument('--cleaned_path', default='./cleaned', type=str)
+
+parser.add_argument('--dr_dialog_sample', default=100, type=int)
+parser.add_argument('--neg_sample', default=10, type=int)
+parser.add_argument('--batch_size', default=256, type=int)
+parser.add_argument('--lr', default=0.001, type=float)
+parser.add_argument('--patience', default=5, type=int)
+parser.add_argument('--output_dir', default="saved_model", type=str)
+parser.add_argument('--epoch_num', default=20, type=int)
+
+
+args = parser.parse_args()
+
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+torch.cuda.set_device(args.gpu)
+
+print(f'Training: randome seed {args.seed}, experiment name: {args.name}, run on gpu {args.gpu}')
+
+def train_model(model, train_dataloader, val_dataloader):
+    print(f'{args.name} start training...')
+    weights = torch.Tensor([1, 5]).cuda()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, eps=1e-8)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=args.patience, threshold=1e-4, min_lr=5e-5)
+    early_stopping = EarlyStopping(patience = args.patience, verbose = True, path = f'{args.output_dir}/ckpt/best_model.pt')
+
+    train_losses = []
+    valid_losses = []
+    for epoch in range(args.epoch_num):
+        print(f'Current Epoch: {epoch+1}')
+        model.train()
+        train_losses = train_epoch(train_dataloader, optimizer, model, "train")
+        model.eval()
+        with torch.no_grad():
+            valid_losses = valid_epoch(val_dataloader, model, "valid")
+        
+        train_loss = np.average(train_losses)
+        valid_loss = np.average(valid_losses)
+                
+        print(f'\nEpoch {epoch+1}, train loss: {train_loss}, valid loss: {valid_loss}')
+        torch.save(model.state_dict(),f'./{args.output_dir}/ckpt/model_{epoch+1}.pt')
+        if (epoch+1 > 15): 
+            early_stopping(valid_loss, model)
+            if early_stopping.early_stop:
+                print(f'Early stopping at epoch {epoch+1}')
+                break  
+        scheduler.step(valid_loss)
+
+def main():
+    print(f'Loadding ids from {args.cleaned_path}...')
+    start = time.time()
+    profile = load_pickle(f'{args.cleaned_path}/profile_embeds.pkl')
+    print("Loaded profile ids")
+    query = load_pickle(f'{args.cleaned_path}/q_embeds.pkl')
+    print("Loaded query ids")
+    dialogue = load_pickle(f'{args.cleaned_path}/dialog_embeds.pkl')
+    print("Loaded dialogue ids")
+    
+    end  = time.time()
+    print("Total loading time: ", end-start, "s")
+    print('Data Statics:')
+    print("The length of profiles: ", len(profile))
+    print("The length of querys: ", len(query))
+    print("The length of dialogues: ", len(dialogue))
+
+    print('Building training dataset and dataloader...')
+    train_set = pd.read_csv(f'./dataset/train_cleaned.csv', delimiter='\t', encoding='utf-8', dtype={'dr_id': str})
+    train_dataset = DoctorRecDataset(
+        'train', train_set, profile, query, dialogue,
+        dr_dialog_sample=args.dr_dialog_sample, neg_sample=args.neg_sample
+    )
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    print('Done')
+
+    print('Building validation dataset and dataloader...')
+    valid_set = pd.read_csv(f'./dataset/valid_cleaned.csv', delimiter='\t', encoding='utf-8', dtype={'dr_id': str})
+    val_dataset = DoctorRecDataset(
+        'valid', valid_set, profile, query, dialogue,
+        dr_dialog_sample=args.dr_dialog_sample, neg_sample=args.neg_sample
+    )
+    val_dataloader = DataLoader(val_dataset, batch_size=101, shuffle=False)
+    print('Done')
+
+    model = ourModel().cuda()
+    if args.n_gpu > 1 and torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = torch.nn.DataParallel(model)
+        model = model.cuda()
+        model = model.module
+    print(next(model.parameters()).device)
+    # with LineProfiler(train_model) as prof:
+    #     train_model(model, train_dataloader, val_dataloader)
+    # prof.display()
+
+    train_model(model, train_dataloader, val_dataloader)
+
+if __name__ == '__main__':
+    main()
